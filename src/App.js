2a2a0bd5929ce38ac77b9e6095dc08d92f2985a3
Connor Heraty <connor.heraty.work@gmail.com>
2021-04-03 16:45:19 -0400

    remove duplicate render function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import * as d3 from "d3";
 import "./App.css";
 
 function App() {
-  // const [data, setData] = useState([]);
-
   useEffect(() => {
     d3.csv(
       "https://raw.githubusercontent.com/connorpheraty/d3-barchart-example/main/src/data.csv"
@@ -30,100 +28,97 @@ function App() {
     const chartTitle = "Countries ranked by GDP";
     const barChartXAxisLabel = "GDP";
 
-    const renderBarChart = (data) => {
-      const xValue = (d) => +d.gdp;
+    const xValue = (d) => +d.gdp;
 
-      const xScale = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, xValue)])
-        .range([0, innerWidth]);
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, xValue)])
+      .range([0, innerWidth]);
 
-      const yScale = d3
-        .scaleBand()
-        .domain(
-          data.map(function (d) {
-            return d.country;
-          })
-        )
-        .range([0, innerHeight])
-        .padding(0.1);
-
-      const tooltip = d3.select("body").append("div").attr("class", "tooltip");
-
-      const g = svg
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .attr("class", "axis");
-
-      g.append("text")
-        .attr("x", innerWidth / 2 - 125)
-        .attr("y", 0 - margin.top / 2)
-        .attr("class", "chart-title")
-        .text(chartTitle);
-
-      const xAxisTickFormat = (number) =>
-        d3.format(".3s")(number).replace("G", "B");
-
-      const xAxis = d3
-        .axisBottom(xScale)
-        .tickFormat(xAxisTickFormat)
-        .tickSize(-innerHeight);
-
-      const xAxisG = g
-        .append("g")
-        .call(xAxis)
-        .attr("transform", `translate(0, ${innerHeight})`);
-
-      xAxisG.select(".domain").remove();
-
-      xAxisG
-        .append("text")
-        .attr("class", "axis-label")
-        .attr("y", 50)
-        .attr("x", innerWidth / 2)
-        .attr("fill", "black")
-        .text(barChartXAxisLabel);
-
-      const yAxis = d3.axisLeft(yScale);
-
-      const yAxisG = g.append("g").call(yAxis);
-
-      yAxisG.selectAll(".tick text").attr("class", "y-axis-ticks");
-
-      yAxisG.select(".domain").remove();
-
-      g.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class", "rect")
-        .attr("y", function (d) {
-          return yScale(d.country) + 8;
+    const yScale = d3
+      .scaleBand()
+      .domain(
+        data.map(function (d) {
+          return d.country;
         })
-        .attr("width", function (d) {
-          return xScale(d.gdp);
-        })
-        .attr("height", yScale.bandwidth() / 2)
-        .on("mouseenter", function (event, d) {
-          d3.select(this).attr("opacity", 0.5);
-        })
-        .on("mouseleave", function (event, d) {
-          d3.select(this).attr("opacity", 1);
-        })
-        .on("mouseover", function () {
-          return tooltip.style("visibility", "visible");
-        })
-        .on("mousemove", function (event, d) {
-          return tooltip
-            .style("top", event.pageY + 30 + "px")
-            .style("left", event.pageX + 20 + "px")
-            .html("GDP: $" + d3.format(".3s")(d.gdp).replace("G", "B"));
-        })
-        .on("mouseout", function () {
-          return tooltip.style("visibility", "hidden");
-        });
-    };
-    renderBarChart(data);
+      )
+      .range([0, innerHeight])
+      .padding(0.1);
+
+    const tooltip = d3.select("body").append("div").attr("class", "tooltip");
+
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .attr("class", "axis");
+
+    g.append("text")
+      .attr("x", innerWidth / 2 - 125)
+      .attr("y", 0 - margin.top / 2)
+      .attr("class", "chart-title")
+      .text(chartTitle);
+
+    const xAxisTickFormat = (number) =>
+      d3.format(".3s")(number).replace("G", "B");
+
+    const xAxis = d3
+      .axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+
+    const xAxisG = g
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0, ${innerHeight})`);
+
+    xAxisG.select(".domain").remove();
+
+    xAxisG
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("y", 50)
+      .attr("x", innerWidth / 2)
+      .attr("fill", "black")
+      .text(barChartXAxisLabel);
+
+    const yAxis = d3.axisLeft(yScale);
+
+    const yAxisG = g.append("g").call(yAxis);
+
+    yAxisG.selectAll(".tick text").attr("class", "y-axis-ticks");
+
+    yAxisG.select(".domain").remove();
+
+    g.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "rect")
+      .attr("y", function (d) {
+        return yScale(d.country) + 8;
+      })
+      .attr("width", function (d) {
+        return xScale(d.gdp);
+      })
+      .attr("height", yScale.bandwidth() / 2)
+      .on("mouseenter", function (event, d) {
+        d3.select(this).attr("opacity", 0.5);
+      })
+      .on("mouseleave", function (event, d) {
+        d3.select(this).attr("opacity", 1);
+      })
+      .on("mouseover", function () {
+        return tooltip.style("visibility", "visible");
+      })
+      .on("mousemove", function (event, d) {
+        return tooltip
+          .style("top", event.pageY + 30 + "px")
+          .style("left", event.pageX + 20 + "px")
+          .html("GDP: $" + d3.format(".3s")(d.gdp).replace("G", "B"));
+      })
+      .on("mouseout", function () {
+        return tooltip.style("visibility", "hidden");
+      });
   };
 
   return (
